a7a286cbb0d6647e4ae4504fa15edc060c234763
DragunWF <plarisanmarc2@gmail.com>
2025-11-06 21:11:26 +0800

    feat(player): implement basic movement

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,18 +1,51 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
 
-public class Player : MonoBehaviour
+public sealed class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private float moveSpeed = 1.5f;
+
+    private Rigidbody2D rigidBody;
+    private SpriteRenderer spriteRenderer;
+    private Animator animator;
+    private Vector2 inputVector;
+
+    private void Awake()
     {
-        
+        rigidBody = GetComponent<Rigidbody2D>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        animator = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        Move();
+        SpriteDirection();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidBody.velocity = inputVector * moveSpeed;
+    }
+
+    private void Move()
+    {
+        inputVector.x = Input.GetAxisRaw("Horizontal");
+        inputVector.y = Input.GetAxisRaw("Vertical");
+        inputVector.Normalize();
+    }
+
+    private void SpriteDirection()
+    {
+        if (inputVector.x > 0)
+        {
+            spriteRenderer.flipX = false;
+        }
+        else if (inputVector.x < 0)
+        {
+            spriteRenderer.flipX = true;
+        }
     }
 }
