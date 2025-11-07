b4e388e3fb861d03583493b59427b497307800df
DragunWF <plarisanmarc2@gmail.com>
2025-11-07 22:53:12 +0800

    fix(player): make the bullet speed consistent regardless of the mouse cursor's distance from the gun

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -69,12 +69,13 @@ public sealed class Player : MonoBehaviour
         var aimAngle = Mathf.Atan2(aimDirection.y, aimDirection.x) * Mathf.Rad2Deg;
         weaponSpriteRenderer.transform.eulerAngles = new Vector3(0, 0, aimAngle);
 
+        // Flip player and weapon sprites based on mouse position
         var isMouseToTheLeft = mousePosition.x < transform.position.x;
         playerSpriteRenderer.flipX = isMouseToTheLeft;
         weaponSpriteRenderer.flipY = isMouseToTheLeft;
 
         // Transform weapon position based on mouse side
-        float weaponPosX = Mathf.Abs(playerWeaponObj.transform.localPosition.x);
+        var weaponPosX = Mathf.Abs(playerWeaponObj.transform.localPosition.x);
         Vector2 weaponPos = new Vector2(
             isMouseToTheLeft ? -weaponPosX : weaponPosX,
             playerWeaponObj.transform.localPosition.y
@@ -87,7 +88,7 @@ public sealed class Player : MonoBehaviour
         var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         var bullet = Instantiate(bulletPrefab, gunTip.position, gunTip.rotation);
         var bulletRigidbody = bullet.GetComponent<Rigidbody2D>();
-        var direction = (Vector2)(mousePosition - gunTip.position).normalized;
-        bulletRigidbody.velocity = direction * bulletSpeed;
+        var direction = (Vector2)(mousePosition - gunTip.position);
+        bulletRigidbody.velocity = direction.normalized * bulletSpeed;
     }
 }
