614322e3d4dc8dedaecd945c009f81f150ab6a20
DragunWF <plarisanmarc2@gmail.com>
2025-11-14 12:07:06 +0800

    refactor(gameStats): implement singleton design pattern for GameStats.cs

--- Assets/Scripts/Core/GameStats.cs
+++ Assets/Scripts/Core/GameStats.cs
@@ -4,44 +4,43 @@ using UnityEngine;
 
 public class GameStats : MonoBehaviour
 {
+    public static GameStats Instance { get; private set; }
+
     [SerializeField] private int points = 0; // Used for purchasing upgrades
     private int enemiesDefeated = 0;
 
-    private GameSceneUI gameSceneUI;
-
     private void Awake()
     {
-        gameSceneUI = FindObjectOfType<GameSceneUI>();
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
     }
 
     public void IncrementEnemiesDefeated()
     {
         enemiesDefeated++;
-        if (gameSceneUI != null)
-        {
-            gameSceneUI.UpdateEnemiesDefeatedUI(enemiesDefeated);
-        }
-        else
-        {
-            Debug.LogError("GameSceneUI reference is missing in GameStats!");
-        }
     }
 
     #region Points Management Methods
 
     public int GetPoints() => points;
+    public int GetEnemiesDefeated() => enemiesDefeated;
 
     public void AddPoints(int amount)
     {
         points += amount;
-        gameSceneUI.UpdatePointsText(points);
     }
 
     public void SubtractPoints(int amount)
     {
         points -= amount;
         if (points < 0) points = 0;
-        gameSceneUI.UpdatePointsText(points);
     }
 
     #endregion
