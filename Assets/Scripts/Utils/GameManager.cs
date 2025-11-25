7c6b82e39a0c05728d5e67d4b6e486c8f4906505
Isaac Severino <isaac.6severino@gmail.com>
2025-11-25 13:11:18 +0800

    main menu: mute toggle & dynamic adjustment

--- Assets/Scripts/Utils/GameManager.cs
+++ Assets/Scripts/Utils/GameManager.cs
@@ -2,12 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public sealed class GameManager : MonoBehaviour
 {
     private const int mainMenuIndex = 0;
     private const int gameSceneIndex = 1;
     private const int gameOverSceneIndex = 2;
+
+    #region Audio
+    [SerializeField] Image soundOnIcon;
+    [SerializeField] Image soundOffIcon;
+    private bool muted;
+    #endregion
 
     #region Load Scene Methods
 
@@ -20,5 +27,26 @@ public sealed class GameManager : MonoBehaviour
     private void LoadScene(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
+    }
+
+    private void Start()
+    {
+
+        muted = PlayerPrefs.GetInt("Muted", 0) == 1;
+        ApplyMuteState();
+    }
+
+    public void ToggleMute()
+    {
+        muted = !muted;
+        PlayerPrefs.SetInt("Muted", muted ? 1 : 0);
+        ApplyMuteState();
+    }
+
+    private void ApplyMuteState()
+    {
+        AudioListener.volume = muted ? 0f : 1f;
+        soundOnIcon.gameObject.SetActive(!muted);
+        soundOffIcon.gameObject.SetActive(muted);
     }
 }
