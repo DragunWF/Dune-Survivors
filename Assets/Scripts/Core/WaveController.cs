c1997cba19f18fcf6bfdd2023a3863fd277f0947
DragunWF <plarisanmarc2@gmail.com>
2025-11-28 11:58:34 +0800

    refactor: balance wave time limits on upper end waves

--- Assets/Scripts/Core/WaveController.cs
+++ Assets/Scripts/Core/WaveController.cs
@@ -25,11 +25,11 @@ public sealed class WaveController : MonoBehaviour
             new(70, 2.2f, 4, 2),  // Wave 3 
             new(90, 2.5f, 4, 2),  // Wave 4 
             new(120, 2.2f, 4, 3),  // Wave 5 
-            new(150, 2.0f, 4, 3),  // Wave 6 
-            new(180, 1.8f, 4, 4), // Wave 7 
-            new(220, 1.8f, 4, 4), // Wave 8 
-            new(250, 1.8f, 4, 5), // Wave 9 
-            new(300, 1.6f, 4, 5)  // Wave 10 
+            new(135, 2.0f, 4, 3),  // Wave 6 
+            new(150, 1.8f, 4, 4), // Wave 7 
+            new(175, 1.8f, 4, 4), // Wave 8 
+            new(225, 1.8f, 4, 5), // Wave 9 
+            new(250, 1.6f, 4, 5)  // Wave 10 
         };
     }
 
