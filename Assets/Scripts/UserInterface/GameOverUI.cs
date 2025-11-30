859ca03967d979662fac79f13b1b9d1130c6622a
Isaac Severino <isaac.6severino@gmail.com>
2025-11-30 14:00:19 +0800

    game over alt art on player win

--- Assets/Scripts/UserInterface/GameOverUI.cs
+++ Assets/Scripts/UserInterface/GameOverUI.cs
@@ -12,22 +12,31 @@ public class GameOverUI : MonoBehaviour
     private const string HIGH_SCORE_TEXT = "HighScoreText"; // Relates to points
     private const string NOTIFICATION_TEXT = "NotificationText";
     private const string ENEMIES_DEFEATED_TEXT = "EnemiesDefeatedText";
+    private const string GAME_OVER_ART = "GameOverArt";
+    private const string PLAYER_WIN_ART = "PlayerWinArt";
+    private const string GAME_OVER_TEXT = "GameOverText";
 
     #endregion
 
     private TextMeshProUGUI pointsText;
     private TextMeshProUGUI enemiesDefeatedText;
     private TextMeshProUGUI highScoreText;
+    private TextMeshProUGUI gameOverText;
     private GameObject notificationTextObj;
+    private GameObject gameOverArt;
+    private GameObject playerWinArt;
 
     private GameStats gameStats;
 
     private void Awake()
     {
+        gameOverText = GameObject.Find(GAME_OVER_TEXT).GetComponent<TextMeshProUGUI>();
         pointsText = GameObject.Find(POINTS_TEXT).GetComponent<TextMeshProUGUI>();
         enemiesDefeatedText = GameObject.Find(ENEMIES_DEFEATED_TEXT).GetComponent<TextMeshProUGUI>();
         highScoreText = GameObject.Find(HIGH_SCORE_TEXT).GetComponent<TextMeshProUGUI>();
         gameStats = FindObjectOfType<GameStats>();
+        gameOverArt = GameObject.Find(GAME_OVER_ART);
+        playerWinArt = GameObject.Find(PLAYER_WIN_ART);
 
         notificationTextObj = GameObject.Find(NOTIFICATION_TEXT);
         notificationTextObj.SetActive(false);
@@ -42,6 +51,20 @@ public class GameOverUI : MonoBehaviour
         if (gameStats.IsNewHighScore)
         {
             notificationTextObj.SetActive(true);
+        }
+
+        gameOverArt.SetActive(false);
+        playerWinArt.SetActive(false);
+
+        if (gameStats.IsGameWin)
+        {
+            gameOverText.text = "You Win!";
+            playerWinArt.SetActive(true);
+        }
+        else
+        {
+            gameOverText.text = "Game Over!";
+            gameOverArt.SetActive(true);
         }
     }
 }
